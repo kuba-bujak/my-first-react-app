3c2c1c25d949c169509ec2d8b7c9a19d0b5edf85
Jakub Bujak <kuba.bujak0@wp.pl>
2023-03-22 10:48:42 +0100

    Create react app and add some profile cards with css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import ProfileCard from './ProfileCard';
+import AlexaImage from './images/alexa.png';
+import CortanaImage from './images/cortana.png';
+import SiriImage from './images/siri.png';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div>
+			<div>Personal Digital Assistants</div>
+
+			<div className='container'>
+				<section className='section'>
+					<div className='columns'>
+
+						<div className='column is-4'>
+							<ProfileCard title="Alexa" handle="@alexa99" image={AlexaImage} />
+						</div>
+
+						<div className='column is-4'>
+							<ProfileCard title="Cortana" handle="@cortana32" image={CortanaImage} />
+						</div>
+
+						<div className='column is-4'>
+							<ProfileCard title="Siri" handle="@siri01" image={SiriImage} />
+						</div>
+
+					</div>
+				</section>
+			</div>
+		</div>
+	);
 }
 
 export default App;
